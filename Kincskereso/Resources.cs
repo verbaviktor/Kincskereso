564fc2cbd698ac112d6ff8af6789bca28fd9bdf2
csizmadia.barnabas <cb20040621@gmail.com>
2023-04-21 10:40:41 +0200

    created Resources and Tools

---
+++ Kincskereso/Resources.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Kincskereso
+{
+    internal class Resources
+    {
+        public Dictionary<TileType, int> resources;
+
+        public Resources()
+        {
+            resources = new Dictionary<TileType, int> {
+                {TileType.Erdő, 3 },
+                {TileType.Hegy, 3 },
+                {TileType.Domb, 3 },
+                {TileType.Ret, 0 },
+            };
+        }
+
+        public bool Add_resource(TileType type) {
+            
+            resources[type] += 1;
+
+            return true;
+        }
+
+        public bool Remove_resource(TileType type) {
+
+            resources[type] -= 1;
+
+            return true;
+        }
+    }
+}
