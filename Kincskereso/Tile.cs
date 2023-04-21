91264d7b10a54e5a893c5419a77e1515dc6cca17
Balint837 <pecsi.balint@students.jedlik.eu>
2023-04-21 09:47:14 +0200

    added dummies, modified gitignore

---
+++ Kincskereso/Tile.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kincskereso
+{
+    internal class Tile
+    {
+        TileType Type;
+        bool[] Direction = new bool[4];
+
+        public Tile(TileType type)
+        {
+            Type = type;
+        }
+
+        public Tile(int type)
+        {
+            Type = (TileType)type;
+        }
+    }
+}
