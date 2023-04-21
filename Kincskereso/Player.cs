445155a5767e6b38b99fe7ebd2fb732d1ff5bfa3
Viktor <slavik00757@gmail.com>
2023-04-21 10:13:51 +0200

    player class

---
+++ Kincskereso/Player.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Kincskereso
+{
+    internal class Player
+    {
+        public Point Position{ get; set; }
+        public Tool  Utcount{ get; set; }
+        public Tool Asocount { get; set; }
+        public bool[5] traversed { get; set; }
+        public int Kincsekcsount { get; set; }
+        public int Lepescount{ get; set; }
+
+
+    }
+}
